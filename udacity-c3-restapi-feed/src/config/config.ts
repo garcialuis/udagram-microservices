242e4873b47891e7c1cdc794f479b3486947fe5b
garcialuis <garcialuisdev@gmail.com>
2020-07-21 00:53:38 -0700

    * Updated config.ts files for backend services (feed/user) to match environmental vars name(s)

--- udacity-c3-restapi-feed/src/config/config.ts
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
+    "username": process.env.UD_DB_USER,
+    "password": process.env.UD_DB_PASSWORD,
+    "database": process.env.UD_DB_DB,
+    "host": process.env.UD_DB_HOST,
     "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL    
+    "aws_reigion": process.env.UD_AWS_REGION,
+    "aws_profile": process.env.UD_AWS_PROFILE,
+    "aws_media_bucket": process.env.UD_AWS_MEDIA_BUCKET,
+    "url": process.env.UD_URL
   },
   "prod": {
     "username": "",
@@ -18,7 +18,7 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": process.env.JWT_SECRET
+    "secret": process.env.API_SECRET
   }
 
 }
